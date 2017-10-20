980c9c52279207a43a52c822bf2f46c39cf48b94
tfreiner <tfreiner@gmail.com>
2017-10-19 21:44:08 -0500

    setting up clock

---
+++ oss.c
@@ -0,0 +1,148 @@
+/**
+ * Author: Taylor Freiner
+ * Date: October 19th, 2017
+ * Log: Setting up clock 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <ctype.h>
+#include <signal.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <time.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <sys/types.h>
+
+int sharedmem[3];
+int processCount = 0;
+int processIds[100];
+
+union semun {
+	int val;
+};
+
+struct controlBlockStruct {
+	int cpuTime;
+	int systemTime;
+	int lastBurstTime;
+	int processPriority;
+} controlBlockStruct[19];
+
+void clean(int sig){
+	fprintf(stderr, "Interrupt signaled. Removing shared memory and killing processes.\n");
+	int i;
+	shmctl(sharedmem[0], IPC_RMID, NULL);
+	shmctl(sharedmem[1], IPC_RMID, NULL);
+	semctl(sharedmem[2], 0, IPC_RMID);
+	for(i = 0; i < processCount; i++){
+		kill(processIds[i], SIGKILL);
+	}
+	exit(1);
+}
+
+int main(int argc, char* argv[])  {
+
+	union semun arg;
+	arg.val = 1;
+	int i;
+
+	//SIGNAL HANDLING
+	signal(SIGINT, clean);
+
+	//FILE MANAGEMENT
+	FILE *file = fopen("log.txt", "w");
+	
+	if(file == NULL){
+		printf("%s: ", argv[0]);
+		perror("Error: \n");
+		return 1;
+	}
+
+	//SHARED MEMORY
+	key_t key = ftok("keygen", 1);
+	key_t key2 = ftok("keygen2", 1);
+	key_t key3 = ftok("keygen3", 1);
+	int memid = shmget(key, sizeof(int*)*2, IPC_CREAT | 0644);
+	int memid2 = shmget(key2, sizeof(struct controlBlockStruct) * 19, IPC_CREAT | 0644);
+	int semid = semget(key3, 1, IPC_CREAT | 0644);
+	if(memid == -1 || memid2 == -1){
+		printf("%s: ", argv[0]);
+		perror("Error\n");
+	}
+	sharedmem[0] = memid;
+	sharedmem[1] = memid2;
+	sharedmem[2] = semid;
+	int *clock = (int *)shmat(memid, NULL, 0);
+	int *controlBlock = shmat(memid2, NULL, 0);
+	if(*clock == -1 || *controlBlock == -1){
+		printf("%s: ", argv[0]);
+		perror("Error\n");
+	}
+	int clockVal = 0;
+	for(i = 0; i < 2; i++){
+		memcpy(&clock[i], &clockVal, 4);
+	}
+	
+	semctl(semid, 0, SETVAL, 1, arg);
+	if(errno){
+		fprintf(stderr, "%s\n", strerror(errno));
+		exit(1);
+	}
+
+	//CREATING PROCESSES
+	int forkTime;
+	int incrementTime;
+	int *lastForkTime;
+	lastForkTime = clock;
+	pid_t childpid;
+	srand(time(NULL));
+	forkTime = rand() % 3;
+	for(i = 0; i < 19; i++){
+		incrementTime = rand() % 1000;
+		printf("forkTime: %d\n", forkTime);
+		printf("incrementTime: %d\n", incrementTime);
+		clock[0] += 1;
+		if(clock[1] + incrementTime >= 1000000000){
+			clock[1] = (clock[1] + incrementTime) % 1000000000;
+			clock[0]++;
+		}
+		else
+			clock[1] += incrementTime;
+
+		if(((clock[0] * 1000000000 + clock[1]) - (lastForkTime[0] * 1000000000 + lastForkTime[1]) > (forkTime * 1000000000))){
+			lastForkTime = clock;
+			forkTime = rand() % 3;
+			childpid = fork();
+			if(errno){
+				fprintf(stderr, "%s", strerror(errno));
+				exit(1);
+			}
+		
+			if(childpid == 0)
+				execl("./user", "user", NULL);
+
+			if(errno){
+				fprintf(stderr, "%s\n", strerror(errno));
+				exit(1);
+			}
+			processIds[i] = childpid;
+			processCount++;
+		}
+	}
+	
+	fclose(file);
+	shmctl(memid, IPC_RMID, NULL);
+	shmctl(memid2, IPC_RMID, NULL);
+	semctl(semid, 0, IPC_RMID);
+
+	for(i = 0; i < processCount; i++)
+		kill(processIds[i], SIGKILL);
+
+	return 0;
+}
---
+++ user.c
@@ -0,0 +1,11 @@
+/**
+ * Author: Taylor Freiner
+ * Date: October 19, 2017
+ * Log: Creating user file
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+	return 0;
+}
